5a5b23435a50d7d84e6a2ea8052c6a4840b0832d
deico <deico@AJesus>
2017-12-03 20:12:08 +0000

    primera prueba

--- gestionHotelServidor/src/main/java/dam/gestionhotelera/MainApp.java
+++ gestionHotelServidor/src/main/java/dam/gestionhotelera/MainApp.java
@@ -5,14 +5,31 @@ import static javafx.application.Application.launch;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 
 public class MainApp extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        
+    Stage PrimarStage = stage;
+        AnchorPane root = FXMLLoader.load(getClass().getResource("/fxml/VPrincipal.fxml"));
+        stage.initStyle(StageStyle.DECORATED);
+        stage.centerOnScreen();
+        stage.setOpacity(0.97);
+        stage.setTitle("Aplicacion Principal");
+        stage.getIcons().add(new Image(getClass().getResource("/images/hotel.png").toString()));
+        Scene scene = new Scene(root);
+        stage.setOnCloseRequest((event) -> {
+            System.exit(0);
+        });
+        stage.setScene(scene);
+        stage.show();
+        stage.setMinHeight(stage.getHeight());
+        stage.setMinWidth(stage.getWidth());       
     }
 
     /**
